00b4050f661c9cd3eff27644977ff60c8b89d62a
Andrey Gluschuk <andryxagl@gmail.com>
2021-09-24 11:12:27 +0300

    Added class Adress, redecorated display for class person

--- Program.cs
+++ Program.cs
@@ -37,18 +37,41 @@ namespace Lab1
         Philosophy
     }
 
+    class Adress
+    {
+        public Adress(string region, string town, string street, string houseNumber, string apartmentNumber)
+        {
+            this.region = region;
+            this.town = town;
+            this.street = street;
+            this.houseNumber = houseNumber;
+            this.apartmentNumber = apartmentNumber;
+        }
+        private string region;
+        private string town;
+        private string street;
+        private string houseNumber;
+        private string apartmentNumber;
+        
+        public string GetAdress()
+        {
+            return $"{region}, {town}, {street}, {houseNumber}, {apartmentNumber}";
+        }
+    }
+
     class Person
     {
         public Person()
         {
                
         }
-        public Person(string Name, string Surname, string Patronymic, int Age)
+        public Person(string Name, string Surname, string Patronymic, int Age, Adress adress)
         {
             this.Name = Name;
             this.Surname = Surname;
             this.Patronymic = Patronymic;
             this.Age = Age;
+            this.Adress = adress.GetAdress();
             ID = Guid.NewGuid();
         }
 
@@ -57,6 +80,7 @@ namespace Lab1
         public string Surname;
         public string Patronymic;
         public int Age;
+        protected string Adress;
 
         public void PrintPersonInfo()
         {
@@ -66,7 +90,9 @@ namespace Lab1
             Console.ResetColor();
             Console.WriteLine($"Id: {ID}");
             Console.WriteLine($"Name: {Name} \t| Surname: {Surname} \t| Patronymic: {Patronymic}");
-            Console.WriteLine($"Age: {Age}\n");
+            Console.WriteLine($"Age: {Age}");
+            Console.WriteLine($"{Adress}\n");
+            
         }
         public string GetFullName()
         {
@@ -76,12 +102,13 @@ namespace Lab1
 
     class Student : Person
     {
-        public Student(string Name, string Surname, string Patronymic, int Age, string group)
+        public Student(string Name, string Surname, string Patronymic, int Age, string group, Adress adress)
         {
             this.Name = Name;
             this.Surname = Surname;
             this.Patronymic = Patronymic;
             this.Age = Age;
+            this.Adress = adress.GetAdress();
             ID = Guid.NewGuid();
             this.group = group;
             faculty = int.Parse(group.Remove(group.IndexOf("-")));
@@ -119,6 +146,7 @@ namespace Lab1
             Console.WriteLine($"Id: {ID}");
             Console.WriteLine($"Name: {Name} \t| Surname: {Surname} \t| Patronymic: {Patronymic}");
             Console.WriteLine($"Age: {Age}");
+            Console.WriteLine(Adress);
             Console.Write($"Faculty: {(Faculty)faculty}\tGroup: {group}\t");
             if (magister)
             {
@@ -137,12 +165,13 @@ namespace Lab1
     class Teacher : Person
     {
         private string Department;
-        public Teacher(string Name, string Surname, string Patronymic, int Age, string Department)
+        public Teacher(string Name, string Surname, string Patronymic, int Age, string Department, Adress adress)
         {
             this.Name = Name;
             this.Surname = Surname;
             this.Patronymic = Patronymic;
             this.Age = Age;
+            this.Adress = adress.GetAdress();
             this.Department = Department;
             ID = Guid.NewGuid();
         }
@@ -155,6 +184,7 @@ namespace Lab1
             Console.WriteLine($"Id: {ID}");
             Console.WriteLine($"Name: {Name} \t| Surname: {Surname} \t| Patronymic: {Patronymic}");
             Console.WriteLine($"Age: {Age}");
+            Console.WriteLine(Adress);
             Console.WriteLine($"Department: {Department}\n");
         }
     }
@@ -165,16 +195,25 @@ namespace Lab1
 
         static void Main(string[] args)
         {
-            Person a = new Person("Andrey", "Gluschuk", "Sergeevich", 19);
-            Person b = new Person(Name: "Ivan", Surname: "Ivanov", "James", 12);
-            Student student = new Student("Student", "KSU", "", 19, "4-145M");
-            Teacher teacher = new Teacher("Teacher", "KSU", "", 26, "Maths");
+            Adress adress1 = new Adress("Khersonska obl.", "Kherson", "Universitetska", "27", "517");
+            Adress adress2 = new Adress("Kyivska obl.", "Kyiv", "Khreshchatyk", "4", "7");
+            Adress adress3 = new Adress("Khersonska obl.", "Kherson", "Universitetska", "27", "510");
+            Adress adress4 = new Adress("Khersonska obl.", "Kherson", "Universitetska", "27", "512");
+
+
+            Person a = new Person("Andrey", "Gluschuk", "Sergeevich", 19, adress1);
+            Person b = new Person(Name: "Ivan", Surname: "Ivanov", "James", 12, adress2);
+            Student student = new Student("Student", "KSU", "", 19, "4-145M", adress3);
+            Teacher teacher = new Teacher("Teacher", "KSU", "", 26, "Maths", adress4);
 
             a.PrintPersonInfo();
             b.PrintPersonInfo();
             student.PrintPersonInfo();
             teacher.PrintPersonInfo();
-            
+
+            Console.WriteLine(adress1.GetAdress());
+            Console.WriteLine(adress2.GetAdress());
+
             string fullnameA = a.GetFullName();
             Console.WriteLine(fullnameA);
 
