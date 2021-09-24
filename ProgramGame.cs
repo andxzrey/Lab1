f735ac339adb0b3a732b5f4eb8d39f7efb46ce1c
Andrey Gluschuk <andryxagl@gmail.com>
2021-09-24 12:37:16 +0300

    Added ProgramGame.cs

---
+++ ProgramGame.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Game
+{
+    class Game
+    {
+        public void Source()
+        {
+            Random rnd = new Random();
+            int rndNum = rnd.Next(0,100);
+            int num = 0;
+            while (num != rndNum)
+            {
+                Console.Write("Enter a num: ");
+                num = int.Parse(Console.ReadLine());
+                if (num < rndNum) { Console.WriteLine("Please enter a number greater."); }
+                if (num > rndNum) { Console.WriteLine("Please enter a number less than what you entered."); }
+            }
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("Congratulations! You did it.");
+            Console.ResetColor();
+        }
+    }
+    class ProgramGame
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Source();
+        }
+    }
+}
